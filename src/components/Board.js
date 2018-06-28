69c22b1a52069476c8501a4f0e4bf9cb5148b054
Derek DiGioia <derekdigioia@gmail.com>
2018-06-28 15:46:13 -0700

    sets up basic state for board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,12 +7,54 @@ import Laser from './Laser'
 class Board extends Component {
   constructor(props) {
     super()
+    this.state = {
+      screen: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        // ratio: window.devicePixelRatio || 1,
+        ratio: 1,
+      },
+      context: null,
+      keys : {
+        left: 0,
+        right: 0,
+        up: 0,
+        down: 0,
+        space: 0,
+      },
+      asteroidCount: 3,
+      currentScore: 0,
+      topScore: localStorage['topscore'] || 0,
+      inGame: false
+    }
+    this.ship = []
+    this.asteroids = []
+    this.lasers = []
+    this.particles = []
   }
+
+  componentDidMount() {
+    console.log('board component mounted')
+    const context = this.refs.canvas.getContext('2d')
+    context.fillStyle = 'white'
+    context.fillRect(100, 100, 100, 100)
+  }
+
+
 
   render() {
     return (
       <div className="Board">
-        Hello. I am a board.
+        <span className="score current-score">Score: {this.currentScore}</span>
+        <span className="score top-score">Top Score: {this.topScore}</span>
+        <span className="controls" >
+          Use [A][S][W][D] or [←][↑][↓][→] to MOVE<br/>
+          Use [SPACE] to SHOOT
+        </span>
+        <canvas ref="canvas"
+          width={this.state.screen.width * this.state.screen.ratio}
+          height={this.state.screen.height * this.state.screen.ratio}
+        />
       </div>
     )
   }
---
+++ src/components/Particle.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Particle extends Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    return(
+      <div className="Particle">
+
+      </div>
+    )
+  }
+}
+
+export default Particle
